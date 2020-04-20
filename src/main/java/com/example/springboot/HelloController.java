4d92e4086380cbdd814b162e9d0f4fbc98bc71df
Ueda Tomomi <tomomi.ueda@flect.local>
2020-04-20 17:18:32 +0900

    'date'

--- src/main/java/com/example/springboot/HelloController.java
+++ src/main/java/com/example/springboot/HelloController.java
@@ -2,12 +2,20 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
+import java.util.Date;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 @RestController
-public class HelloController {
+public cimport ss HelloController {
 
 	@RequestMapping("/")
 	public String index() {
+		Date date=new Date();
+		DateFormat dateFormat=new SimpleDateFormat("yyyy-MM-dd HH:mm");
+		String formattedDate=dateFormat.format(date);
+
+		System.out.println(formattedDate);
 		return "Greetings from Spring Boot!";
 	}
 
