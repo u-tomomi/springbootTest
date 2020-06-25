aecd6e7be724afbd2ba6768bb95c9a79cdf6a18f
tomomi ueda <tomomi.ueda@flect.co.jp>
2020-06-25 17:13:01 +0900

    修正

--- src/main/java/org/springframework/security/samples/config/HttpSessionConfig.java
+++ src/main/java/org/springframework/security/samples/config/HttpSessionConfig.java
@@ -16,8 +16,14 @@
 
 package org.springframework.security.samples.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @EnableRedisHttpSession
 public class HttpSessionConfig {
+	@Bean
+    public ConfigureRedisAction configureRedisAction() {
+        return ConfigureRedisAction.NO_OP;
+    }
 }
