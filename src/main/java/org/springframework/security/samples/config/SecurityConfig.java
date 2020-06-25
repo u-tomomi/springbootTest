4ef12b1fc03a262b85728a5f74d4a446d486a1e7
tomomi ueda <tomomi.ueda@flect.co.jp>
2020-06-25 17:36:53 +0900

    修正

--- src/main/java/org/springframework/security/samples/config/SecurityConfig.java
+++ src/main/java/org/springframework/security/samples/config/SecurityConfig.java
@@ -16,16 +16,23 @@
 package org.springframework.security.samples.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
  * @author Joe Grandja
  */
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	@Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 
 	// @formatter:off
 	@Override
